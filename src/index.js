91ff20c03decfaf3a30eda8955708f559e1c837b
Mykola Kovalchuk <kolyankov@gmail.com>
2023-09-27 16:37:42 +0300

    Updated imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+import photoService from './js/photo-service';
+import { loadMoreButton, searchQueryButton } from './js/load-more-button';
+import initializePhotoCard from './js/templates';
+import refs from './js/get-refs';
